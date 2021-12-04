f679d49fa314514890aa7f75ee7c668606313684
Musiker15 <info@musiker15.de>
2021-12-04 15:15:12 +0100

    Big Update v2.4.3
    + Fixed a lot of Bugs
    + Rewrite config.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'Musiker15'
+description 'ESX Better Engine Toggle'
+
+client_scripts {
+    'config.lua',
+	'client/client.lua',
+}
+
+server_scripts {
+    'config.lua',
+	'server/server.lua',
+}
