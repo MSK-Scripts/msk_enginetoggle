90d7cb3c19bc8830368766c2d1d1a8c5599cb6a3
Musiker15 <info@musiker15.de>
2021-12-20 19:45:12 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -5,3 +5,22 @@ if Config.UseCommand then
 		end
 	end, false)
 end
+
+local CurrentVersion = '2.5'
+local GithubResourceName = 'EngineToggle'
+
+PerformHttpRequest('https://raw.githubusercontent.com/Musiker15/EngineToggle/main/VERSION', function(Error, NewestVersion, Header)
+	print("\n###############################")
+	print('## ' .. GetCurrentResourceName())
+	print('')
+	print('## Current Version: ' .. CurrentVersion)
+	print('## Newest Version: ' .. NewestVersion)
+	if CurrentVersion < NewestVersion then
+		print('## Outdated')
+		print('## Download here: https://github.com/Musiker15/EngineToggle/releases')
+		print("###############################\n")
+	elseif CurrentVersion == NewestVersion then
+		print('## Up to date!')
+		print("###############################\n")
+	end
+end)
