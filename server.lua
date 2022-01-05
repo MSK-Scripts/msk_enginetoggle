c4d0f71d406751923c3a0779228e688588466823
Musiker15 <info@musiker15.de>
2022-01-06 00:58:14 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -10,30 +10,34 @@ end
 
 local CurrentVersion = '2.6.5'
 local resourceName = "\x1b[32m["..GetCurrentResourceName().."]\x1b[0m"
+local VehicleKeyChain = "\x1b[32m[VehicleKeyChain]\x1b[0m"
 
 PerformHttpRequest('https://raw.githubusercontent.com/Musiker15/EngineToggle/main/VERSION', function(Error, NewestVersion, Header)
 	print("\n###############################")
-	print('## ' .. resourceName)
-	print('')
-	print('## Current Version: ' .. CurrentVersion)
-	print('## Newest Version: ' .. NewestVersion)
-	print('')
-	if Config.VehicleKeyChain then
-		if (GetResourceState("VehicleKeyChain") == "started") then
-			print('## [READY] Script "VehicleKeyChain" found!')
-			print('')
-		elseif (GetResourceState("VehicleKeyChain") == "stopped") then
-			print('## [ERROR] Script "VehicleKeyChain" not found!')
-			print('## Please be sure, that VehicleKeyChain is started.')
-			print('')
+    if CurrentVersion == NewestVersion then
+	    print(resourceName .. 'Resource is Up to Date')
+	    print('## Current Version: ' .. CurrentVersion)
+		print('')
+		if Config.VehicleKeyChain then
+			if (GetResourceState("VehicleKeyChain") == "started") then
+				print('## [READY] Script '.. VehicleKeyChain ..' found!')
+			elseif (GetResourceState("VehicleKeyChain") == "stopped") then
+				print('## [ERROR] Script '.. VehicleKeyChain ..' not found! Please be sure, that '.. VehicleKeyChain ..' is started.')
+			end
 		end
-	end
-	if CurrentVersion ~= NewestVersion then
-		print('## Outdated')
-		print('## Download Newest Version here: https://github.com/Musiker15/EngineToggle/releases')
-		print("###############################\n")
-	elseif CurrentVersion == NewestVersion then
-		print('## Up to Date')
-		print("###############################\n")
-	end
+    elseif CurrentVersion ~= NewestVersion then
+        print(resourceName .. 'Resource Outdated. Please Update!')
+		print('## Current Version: ' .. CurrentVersion)
+	    print('## Newest Version: ' .. NewestVersion)
+        print('## Download Newest Version here: https://github.com/Musiker15/EngineToggle/releases')
+		print('')
+		if Config.VehicleKeyChain then
+			if (GetResourceState("VehicleKeyChain") == "started") then
+				print('## [READY] Script '.. VehicleKeyChain ..' found!')
+			elseif (GetResourceState("VehicleKeyChain") == "stopped") then
+				print('## [ERROR] Script '.. VehicleKeyChain ..' not found! Please be sure, that '.. VehicleKeyChain ..' is started.')
+			end
+		end
+    end
+	print("###############################\n")
 end)
