e757e86566fd6cf199deba515feeb1b856ab4bb3
Musiker15 <info@musiker15.de>
2022-01-16 03:22:10 +0100

    Update server.lua
    Update VersionChecker

--- server.lua
+++ server.lua
@@ -13,34 +13,27 @@ function GetCurrentVersion()
 end
 
 local CurrentVersion = GetCurrentVersion()
-local resourceName = "^2["..GetCurrentResourceName().."]^0"
+local resourceName = "^4["..GetCurrentResourceName().."]^0"
 local VehicleKeyChain = "^3[VehicleKeyChain]^0"
 
 PerformHttpRequest('https://raw.githubusercontent.com/Musiker15/EngineToggle/main/VERSION', function(Error, NewestVersion, Header)
 	print("###############################")
     if CurrentVersion == NewestVersion then
-	    print(resourceName .. ' Resource is Up to Date.')
-	    print('Current Version: ^2' .. CurrentVersion .. '^0')
+	    print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
 		if Config.VehicleKeyChain then
 			if (GetResourceState("VehicleKeyChain") == "started") then
-				print('')
-				print('## [READY] Script '.. VehicleKeyChain ..' found!')
+				print('^2[READY]^0 Script '.. VehicleKeyChain ..' found!')
 			elseif (GetResourceState("VehicleKeyChain") == "stopped") then
-				print('')
-				print('## [ERROR] Script '.. VehicleKeyChain ..' not found! Please be sure, that '.. VehicleKeyChain ..' is started.')
+				print('^1[ERROR]^0 Script '.. VehicleKeyChain ..' not found! Please be sure, that '.. VehicleKeyChain ..' is started.')
 			end
 		end
     elseif CurrentVersion ~= NewestVersion then
-        print(resourceName .. ' Resource Outdated. Please Update!')
-		print('Current Version: ^1' .. CurrentVersion .. '^0')
-	    print('Newest Version: ^2' .. NewestVersion .. '^0')
-        print('Download Newest Version here: https://github.com/Musiker15/EngineToggle/releases')
+        print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
+        print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here: ^9https://github.com/Musiker15/EngineToggle/releases/tag/v'.. NewestVersion .. '^0')
 		if Config.VehicleKeyChain then
 			if (GetResourceState("VehicleKeyChain") == "started") then
-				print('')
 				print('^2[READY]^0 Script '.. VehicleKeyChain ..' found!')
 			elseif (GetResourceState("VehicleKeyChain") == "stopped") then
-				print('')
 				print('^1[ERROR]^0 Script '.. VehicleKeyChain ..' not found! Please be sure, that '.. VehicleKeyChain ..' is started.')
 			end
 		end
