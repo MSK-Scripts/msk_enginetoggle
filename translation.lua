eb248b76d9a667c2be8b9c409878ef3ae12b1a41
Musiker15 <info@musiker15.de>
2022-06-04 17:45:48 +0200

    Create translation.lua

---
+++ translation.lua
@@ -0,0 +1,64 @@
+Translation = {}
+----------------------------------------------------------------
+Translation = {
+    ['de'] = {
+        ['notification_header'] = 'Dein Fahrzeug',
+        ['n_engine_start'] = 'Der Motor wurde gestartet',
+        ['n_engine_stop'] = 'Der Motor wurde gestoppt',
+        ['okok_engine_start'] = "Der Motor wurde <span style='color:#47cf73'>gestartet</span>",
+        ['okok_engine_stop'] = "Der Motor wurde <span style='color:#FF0000'>ausgeschalten</span>",
+        ['engine_start'] = 'Der Motor wurde ~g~gestartet',
+        ['engine_stop'] = 'Der Motor wurde ~r~gestoppt',
+
+        ['n_key_nokey'] = 'Du besitzt keinen Schlüssel für dieses Fahrzeug',
+        ['okok_key_nokey'] = "Du besitzt keinen <span style='color:#FF0000'>Schlüssel</span> für dieses Fahrzeug",
+        ['key_nokey'] = 'Du besitzt ~r~keinen~s~ Schlüssel für dieses Fahrzeug',
+
+        ['n_engine_onatenter'] = 'Der Motor läuft bereits',
+        ['okok_engine_onatenter'] = 'Der Motor läuft bereits',
+        ['engine_onatenter'] = 'Der Motor läuft bereits',
+
+        ['header'] = 'Fremdes Fahrzeug',
+        ['hasno_lockpick'] = 'Du hast keinen Dietrich dabei',
+        ['hotwiring'] = 'Fahrzeug wird geknackt...',
+        ['vehicle_unlocked'] = 'Fahrzeug geknackt',
+        ['hotwiring_failed'] = 'Dietrich ist abgebrochen',
+        ['hotwire_activesearchkey'] = 'Du suchst nach dem Fahrzeugschlüssel...',
+        ['hotwiring_foundkey'] = 'Fahrzeugschlüssel gefunden',
+        ['hotwiring_notfoundkey'] = 'Fahrzeugschlüssel nicht gefunden',
+
+        ['n_hotwire_searchkey'] = "Drücke <span style='color:#47cf73'>E</span> um den Fahrzeugschlüssel zu suchen",
+        ['okok_hotwire_searchkey'] = "Drücke <span style='color:#47cf73'>E</span> um den Fahrzeugschlüssel zu suchen",
+        ['hotwire_searchkey'] = 'Drücke ~r~E~s~ um den Fahrzeugschlüssel zu suchen',
+    },
+    ['en'] = {
+        ['notification_header'] = 'Personal Vehicle',
+        ['n_engine_start'] = 'The Engine started',
+        ['n_engine_stop'] = 'The Engine stopped',
+        ['okok_engine_start'] = 'The Engine <span style="color:#47cf73">started</span>',
+        ['okok_engine_stop'] = 'The Engine <span style="color:#FF0000">stopped</span>',
+        ['engine_start'] = 'The Engine ~g~started',
+        ['engine_stop'] = 'The Engine ~r~stopped',
+
+        ['n_key_nokey'] = 'You do not have a Key for this Vehicle',
+        ['okok_key_nokey'] = 'You do not have a <span style="color:#FF0000">Key</span> for this Vehicle',
+        ['key_nokey'] = 'You do ~r~not~s~ have a Key for this Vehicle',
+
+        ['n_engine_onatenter'] = 'The Engine is running',
+        ['okok_engine_onatenter'] = 'The Engine is running',
+        ['engine_onatenter'] = 'The Engine is running',
+
+        ['header'] = 'Fremdes Fahrzeug',
+        ['hasno_lockpick'] = 'Du hast keinen Dietrich dabei',
+        ['hotwiring'] = 'Fahrzeug wird geknackt...',
+        ['vehicle_unlocked'] = 'Fahrzeug geknackt',
+        ['hotwiring_failed'] = 'Dietrich ist abgebrochen',
+        ['hotwire_activesearchkey'] = 'Du suchst nach dem Fahrzeugschlüssel...',
+        ['hotwiring_foundkey'] = 'Fahrzeugschlüssel gefunden',
+        ['hotwiring_notfoundkey'] = 'Fahrzeugschlüssel nicht gefunden',
+
+        ['n_hotwire_searchkey'] = "Drücke <span style='color:#47cf73'>E</span> um den Fahrzeugschlüssel zu suchen",
+        ['okok_hotwire_searchkey'] = "Drücke <span style='color:#47cf73'>E</span> um den Fahrzeugschlüssel zu suchen",
+        ['hotwire_searchkey'] = 'Drücke ~r~E~s~ um den Fahrzeugschlüssel zu suchen',
+    },
+}
